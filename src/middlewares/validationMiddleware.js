51dbae86afc9260ce33f60004c7d2d12af60b97e
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2024-10-31 17:28:04 +0300

    password regex updated

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -30,7 +30,7 @@ const signupSchema = Joi.object({
         'any.required': 'Email is required'
     }),
     password: Joi.string()
-        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/)
         .message('Password must be at least 8 characters long and contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character (@$!%*?&)')
         .required(),
     phone_number: Joi.string().optional(),
