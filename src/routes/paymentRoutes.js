7f5172aab65ba908a69804325411a4af0bac6d06
RosuMihaiAlexandru <admin@sundust.ai>
2024-12-13 00:05:10 +1000

    Add paypal payment routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -7,4 +7,7 @@ const router = express.Router();
 router.post('/stripe/subscription', authenticateToken, validateCreateStripeSubscription, paymentController.createStripeSubscription);
 router.delete('/stripe/subscription/:id', authenticateToken, paymentController.cancelStripeSubscription);
 
+router.post('/paypal/subscription', authenticateToken, paymentController.cancelPayPalSubscription);
+router.post('/paypal/subscription/:id', authenticateToken, paymentController.cancelPayPalSubscription);
+
 module.exports = router; 
